1cfccd51fe60416db7107708c6c926ac1002571c
Azat S <to@azat.io>
2017-01-12 18:36:47 +0300

    Update e-mail regular expression

--- source/regexp/email.js
+++ source/regexp/email.js
@@ -6,5 +6,5 @@
  *                      expression and the specified string; otherwise, false.
  */
 export default function matchEmail(str) {
-    return /[a-z0-9._%+-]@[a-z0-9-]+.+.[a-z]{2,6}/i.test(str);
+    return /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,8}))$/i.test(str);
 }
